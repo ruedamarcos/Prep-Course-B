f7571197710b5f205062f8221db58e8bb6f7c99c
marcos rueda <mitsuki3636@gmail.com>
2022-10-17 16:51:51 -0300

    Tercer commit

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -5,42 +5,64 @@
 // No cambies los nombres de las funciones.
 
 function imprimirSumaNumeros() {
-    // Imprime la suma de los números del 1 al 10
-    // Pista: usa un acumulador
-    // Tu código:
+  // Imprime la suma de los números del 1 al 10
+  // Pista: usa un acumulador
+  // Tu código:
+  var suma = 0;
+  for (var i = 1; i <= 10; i++) {
+    suma = suma + i;
+  }
+  return suma;
 }
 
-function encuentraPares(array){
+function encuentraPares(array) {
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+  //con while y filther(probar)
+  var resultado = [];
+  var indice = 0;
+  while (indice < array.length) {
+    if (array[indice] % 2 === 0) {
+      resultado.push(array[indice]);
+    }
+    indice++;
+  }
+  return resultado;
 }
 
-function elevaAlCuadrado(array){
+function elevaAlCuadrado(array) {
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+  return array.map(function (num) {
+    return Math.pow(num, 2);
+  });
 }
 
-function sumaArray(array){
+function sumaArray(array) {
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+  return array.reduce(function (acumulador, num) {
+    return acumulador + num;
+  }, 0);
 }
 
-function numeroDigitos(num){
+function numeroDigitos(num) {
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  var numeroAString = num + "";
+  return numeroAString.length;
 }
-  
-  
-  // No modificar nada debajo de esta línea
-  // --------------------------------
-  
-  module.exports = {
-    imprimirSumaNumeros,
-    encuentraPares,
-    elevaAlCuadrado,
-    sumaArray,
-    numeroDigitos
-  };
+
+// No modificar nada debajo de esta línea
+// --------------------------------
+
+module.exports = {
+  imprimirSumaNumeros,
+  encuentraPares,
+  elevaAlCuadrado,
+  sumaArray,
+  numeroDigitos,
+};
